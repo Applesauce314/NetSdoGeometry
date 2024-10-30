81d1c49d86ca50f6c08dbd6dc6bbf24d9fb1fb4e
apmoskevitz <andrew.moskevitz@gdbiw.com>
2024-10-30 12:10:07 -0400

    added missing using

--- NetSdoGeometry/OracleArrayTypeFactory.cs
+++ NetSdoGeometry/OracleArrayTypeFactory.cs
@@ -1,8 +1,10 @@
+using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
+
+using System;
 
 namespace NetSdoGeometry
 {
-    using System;
 
     public abstract class OracleArrayTypeFactoryBase<T> : IOracleArrayTypeFactory
     {
